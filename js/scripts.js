73b1fe8a87af44de1d0b5f6190e061fd344800a4
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-09-24 17:32:03 +0200

    add pokemon objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-var favoriteFood = 'steak';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList.push({
+  name: 'Bulbasaur',
+  height: 7,
+  types: ['grass', 'poison'],
+});
+
+pokemonList.push({
+  name: 'Charmander',
+  height: 6,
+  types: ['fire'],
+});
+
+pokemonList.push({
+  name: 'Squirtle',
+  height: 5,
+  types: ['water'],
+});
