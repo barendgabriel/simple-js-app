822e3b49ccbfd4bdd5c029b5c80fd423204b1f9e
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-10-03 14:34:02 +0200

    Update styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  // Fetch Pokémon from the API
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon); // Add the Pokémon to the pokemonList
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -9,35 +30,39 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  // Create list item
+  function addListItem(pokemon) {
+    let pokemonListElement = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    pokemonListElement.appendChild(listItem);
+
+    // Add event listener to show details
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+    // Later, you'll add code here to show details (e.g., height, types) in a modal.
+  }
+
   return {
-    add: add,
     getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList,
   };
 })();
 
-pokemonRepository.add({
-  name: 'Bulbasaur',
-  height: 7,
-  types: ['grass', 'poison'],
-});
-
-pokemonRepository.add({
-  name: 'Charmander',
-  height: 6,
-  types: ['fire'],
-});
-
-pokemonRepository.add({
-  name: 'Squirtle',
-  height: 5,
-  types: ['water'],
-});
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  let thresholdHeight = 6;
-  let output = `${pokemon.name} (height: ${pokemon.height})`;
-  if (pokemon.height > thresholdHeight) {
-    output += " - Wow, that's big!";
-  }
-  document.write(output + '<br>');
+// Load and display Pokémon
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
