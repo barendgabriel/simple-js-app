2a495ecdc10a5924b5ca2afcae9bcb36351d5260
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-10-03 08:45:59 +0200

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,20 @@
 let pokemonRepository = (function () {
-  // Private pokemonList inside the IIFE
   let pokemonList = [];
 
-  // Public function to add a Pokémon to the list
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
-  // Public function to get all Pokémon
   function getAll() {
     return pokemonList;
   }
 
-  // Return the public functions to be accessible outside
   return {
     add: add,
     getAll: getAll,
   };
 })();
 
-// Add Pokémon to the repository using the add function
 pokemonRepository.add({
   name: 'Bulbasaur',
   height: 7,
@@ -38,7 +33,6 @@ pokemonRepository.add({
   types: ['water'],
 });
 
-// Use forEach() to iterate over the pokemonList and display the details
 pokemonRepository.getAll().forEach(function (pokemon) {
   let thresholdHeight = 6;
   let output = `${pokemon.name} (height: ${pokemon.height})`;
